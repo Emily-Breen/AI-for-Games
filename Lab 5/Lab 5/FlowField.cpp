0c348934c2677dadbe436a54d474ac42430ceeee
Emily-Breen <c00288668@setu.ie>
2025-10-23 21:40:25 +0100

    uploaded lab 05 including start circle and goal circle

--- Lab 5/Lab 5/FlowField.cpp
+++ Lab 5/Lab 5/FlowField.cpp
@@ -4,15 +4,35 @@ FlowField::FlowField()
 {
 	m_grid.setPrimitiveType(sf::PrimitiveType::Lines);
 	makeGrid();
+
+	m_circle.setRadius(8.f);
+	m_circle.setFillColor(sf::Color::Red);
+	m_circle.setOrigin(sf::Vector2f(m_circle.getRadius(), m_circle.getRadius()));
+
+	m_goalCircle.setRadius(8.f);
+	m_goalCircle.setFillColor(sf::Color::Green);
+	m_goalCircle.setOrigin(sf::Vector2f(m_goalCircle.getRadius(), m_goalCircle.getRadius()));
+	
+
+	float dx = screenWidth / cols;
+	float dy = screenHeight / rows;
+
+	float centerX = (cellX + 0.5f) * dx;
+	float centerY = (cellY + 0.5f) * dy;
+	float goalCenterX = (cellGoalX + 0.5f) * dx;
+	float goalCenterY = (cellGoalY + 0.5f) * dy;
+
+	m_circle.setPosition(sf::Vector2f(centerX,centerY));
+	m_goalCircle.setPosition(sf::Vector2f(goalCenterX, goalCenterY));
+
+}
+
+void FlowField::update(float dt)
+{
 }
 
 void FlowField::makeGrid()
 {
-	int cols = 50;
-	int rows = 50;
-	float screenWidth = 1920;
-	float screenHeight = 1920;
-
 	float dx = screenWidth / cols;
 	float dy = screenHeight / rows;
 
@@ -31,7 +51,14 @@ void FlowField::makeGrid()
 	}
 }
 
-void FlowField::draw(sf::RenderTarget& target, sf::RenderStates states) const
+void FlowField::draw(sf::RenderWindow& m_window)
 {
-	target.draw(m_grid, states);
+	m_window.draw(m_grid);
+	m_window.draw(m_circle);
+	m_window.draw(m_goalCircle);
+}
+
+sf::Vector2f FlowField::getDirectionsAtPosition(sf::Vector2f position)
+{
+	return sf::Vector2f();
 }
--- Lab 5/Lab 5/FlowField.h
+++ Lab 5/Lab 5/FlowField.h
@@ -1,15 +1,35 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
-class FlowField : public sf::Drawable
+class FlowField
 {
 public:
 	FlowField();
+	void update(float dt);
+	void draw(sf::RenderWindow& m_window);
 
 private:
 	sf::VertexArray m_grid;
 	void makeGrid();
-	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+	
+	sf::Vector2f getDirectionsAtPosition(sf::Vector2f position);
+	float cellSize = 21.6f;
+	float screenWidth = 1080;
+	float screenHeight = 1080;
+	int cellX = 5;
+	int cellY = 5;
+	int cellGoalX = 45;
+	int cellGoalY = 45;
+	int cols = 50;
+	int rows = 50;
+	float dx;
+	float dy;
+	sf::CircleShape m_circle;
+	sf::CircleShape m_goalCircle;
+	sf::Vector2f m_goal;
+
+
+
 
 };
 
