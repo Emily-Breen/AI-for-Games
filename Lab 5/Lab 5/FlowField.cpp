68fa182df4e339f761be634327a4ca7a2887b72c
Emily-Breen <c00288668@setu.ie>
2025-11-13 17:53:33 +0000

    added in comments

--- Lab 5/Lab 5/FlowField.cpp
+++ Lab 5/Lab 5/FlowField.cpp
@@ -55,21 +55,21 @@ void FlowField::update(float dt)
 
 	sf::Vector2f position = m_circle.getPosition();
 
-	
+	//get center of current cell
 	int cellX = static_cast<int>(position.x / cellSize);
 	int cellY = static_cast<int>(position.y / cellSize);
 	sf::Vector2f cellCenter((cellX + 0.5f) * cellSize, (cellY + 0.5f) * cellSize);
 
-	
+	//looks up direction from flow field if not an obstacle or higher cost dont move into it
 	sf::Vector2f direction = getDirectionsAtPosition(position);
 	if (direction.x == 0.f && direction.y == 0.f)
 		return;
-
+	//normalize to 1 the move in that direction
 	direction = MathUtils::normalize(direction);
 	float speed = 100.f;
 	sf::Vector2f nextPos = position + direction * speed * dt;
 
-	
+	//just to keep circle in center of cell when changing direction
 	int nextCellX = static_cast<int>(nextPos.x / cellSize);
 	int nextCellY = static_cast<int>(nextPos.y / cellSize);
 	sf::Vector2f nextCellCenter((nextCellX + 0.5f) * cellSize, (nextCellY + 0.5f) * cellSize);
@@ -95,6 +95,7 @@ void FlowField::update(float dt)
 
 void FlowField::makeGrid()
 {
+	//makes the grid lines (cols and rows)
 	float dx = screenWidth / cols;
 	float dy = screenHeight / rows;
 
@@ -149,6 +150,7 @@ void FlowField::drawVectorField(sf::RenderWindow& m_window)
 
 	const float arrowLen = cellSize * 0.4f;
 	const float arrowSize = cellSize * 0.3f;
+	// 135 degrees in radians for arrowhead directions
 	const float angle = 3.14159f * 3.f / 4.f;
 
 	const float cosA = cos(angle);
@@ -160,7 +162,7 @@ void FlowField::drawVectorField(sf::RenderWindow& m_window)
 		{
 			int index = y * cols + x;
 			sf::Vector2f direction = m_directions[index];
-
+			//if no direction no arrow
 			if (direction.x == 0.f && direction.y == 0.f)
 				continue;
 
@@ -196,6 +198,7 @@ void FlowField::drawVectorField(sf::RenderWindow& m_window)
 }
 void FlowField::drawPath(sf::RenderWindow& m_window)
 {
+	//just to draw the path taken by the circle if its heatmap draws in cyan
 	sf::RectangleShape pathTile(sf::Vector2f(cellSize, cellSize));
 	pathTile.setFillColor(sf::Color(sf::Color::Magenta));
 	if (showHeatMap ==true) {
@@ -212,12 +215,12 @@ void FlowField::drawHeatMap(sf::RenderWindow& m_window)
 	sf::RectangleShape tile(sf::Vector2f(cellSize, cellSize));
 
 	float maxIntegration = 1.f;
-
+	//finds the max integration for everything thats not an obstacle
 	for (int i = 0; i < (int)m_integration.size(); ++i)
 		if (m_costs[i] < 9999.f && m_integration[i] > maxIntegration)
 			maxIntegration = m_integration[i];
 	if (maxIntegration < 1.f) maxIntegration = 1.f;
-
+	//loop so if 0 its near the goal (blue) if max its far from goal (red)
 	for (int y = 0; y < rows; ++y)
 	{
 		for (int x = 0; x < cols; ++x)
@@ -244,6 +247,7 @@ void FlowField::drawHeatMap(sf::RenderWindow& m_window)
 
 void FlowField::drawObstacles(sf::RenderWindow& m_window)
 {
+	//draws black tiles for obstacles
 	sf::RectangleShape obstacleTile(sf::Vector2f(cellSize, cellSize));
 	obstacleTile.setFillColor(sf::Color::Black);
 
@@ -282,9 +286,10 @@ void FlowField::toggleObstacleAtPosition(sf::Vector2f position)
 		return;
 
 	int index = y * cols + x;
+	//toggle between obstacle and normal cost
 	m_costs[index] = (m_costs[index] == 1.f) ? 9999.f : 1.f;
 
-	
+	//updates the field
 	fieldNeedsUpdate = true;
 
 }
@@ -307,7 +312,7 @@ std::vector<sf::Vector2f> FlowField::getNeighbours(sf::Vector2f cell)
 {
 
 	std::vector<sf::Vector2f> neighbours;
-
+	//check all 8 possible neighbours if 0,0 thats where you are now, gets rid of neighbours outside grid
 	for (int j = -1; j <= 1; ++j)
 	{
 		for (int i = -1; i <= 1; ++i)
@@ -322,7 +327,7 @@ std::vector<sf::Vector2f> FlowField::getNeighbours(sf::Vector2f cell)
 			if (neighbourX < 0 || neighbourX >= cols || neighbourY < 0 || neighbourY >= rows) {
 				continue;
 			}
-
+			//this stops cutting corners through obstacles so like if moving diagonally and one of the adjacent cells is an obstacle it wont allow it
 			if (std::abs(i) == 1 && std::abs(j) == 1)
 			{
 				int adjacent1 = neighbourY * cols + static_cast<int>(cell.x);
@@ -340,12 +345,14 @@ std::vector<sf::Vector2f> FlowField::getNeighbours(sf::Vector2f cell)
 
 float FlowField::heuristic(sf::Vector2f from, sf::Vector2f to)
 {
+	//from to to scaled by cell size
 	sf::Vector2f diff = to - from;
 	return MathUtils::vectorLength(diff) * cellSize;
 }
 
 sf::Vector2f FlowField::getDirectionsAtPosition(sf::Vector2f position)
 {
+	//gets the direction from the flow field at a given position
 	int x = (int)(position.x / cellSize);
 	int y = (int)(position.y / cellSize);
 	if (x < 0 || x >= cols || y < 0 || y >= rows)
@@ -361,13 +368,9 @@ sf::Vector2f FlowField::getDirectionsAtPosition(sf::Vector2f position)
 //movement cost field for each cell in the grid
 void FlowField::makeCostsField()
 {
+	//sets to 1.f cost
 	m_costs.resize(cols * rows, 1.f);
 
-	for (int y = 10; y < 15; ++y) {
-		for (int x = 10; x < 15; ++x) {
-			m_costs[y * cols + x] = 8.f;
-		}
-	}
 }
 
 void FlowField::makeIntegrationFieldBFS()
@@ -379,23 +382,24 @@ void FlowField::makeIntegrationFieldBFS()
 	}
 	else{
 		std::cout << "Using BFS";
+		//sets all to high cost
 		m_integration.assign(cols * rows, 999999.f);
 
 		int goalIndex = cellGoalY * cols + cellGoalX;
 		if (goalIndex < 0 || goalIndex >= cols * rows)
 			return;
-
+		// marks it at the bottom (low)
 		m_integration[goalIndex] = 0.f;
-
+		
 		std::queue<sf::Vector2f> open;
 		open.push(sf::Vector2f(static_cast<float>(cellGoalX), static_cast<float>(cellGoalY)));
-
+		
 		while (!open.empty())
 		{
 			auto current = open.front();
 			open.pop();
 			int currentIndex = current.y * cols + current.x;
-
+			//for each neighbour get current valdi neighbour
 			for (auto& neighbor : getNeighbours(sf::Vector2f((float)current.x, (float)current.y)))
 			{
 				int neighbourX = (int)neighbor.x;
@@ -405,12 +409,12 @@ void FlowField::makeIntegrationFieldBFS()
 				if (m_costs[neighbourIndex] >= 9999.f)
 					continue;
 
-				// Euclidean distance calculation
+				// Euclidean distance calculation what cost to reach current cell and cost of moving to this neighbour
 				sf::Vector2f offset = neighbor - current;
 				float euclideanDistance = MathUtils::vectorLength(offset) * cellSize;
 
 				float newCost = m_integration[currentIndex] + m_costs[neighbourIndex] + euclideanDistance;
-
+				//if its lower update and add to open list
 				if (newCost < m_integration[neighbourIndex])
 				{
 					m_integration[neighbourIndex] = newCost;
@@ -425,6 +429,7 @@ void FlowField::makeIntegrationFieldBFS()
 
 void FlowField::makeIntegrationFieldAStar()
 {
+	//same as bfs but using a priority queue and heuristic goal starts at 0 and rest massive
      m_integration.assign(cols * rows, 999999.f);
      int goalIndex = cellGoalY * cols + cellGoalX;
    if (goalIndex < 0 || goalIndex >= cols * rows)
@@ -432,38 +437,41 @@ void FlowField::makeIntegrationFieldAStar()
    m_integration[goalIndex] = 0.f;
 
   std::priority_queue<Node, std::vector<Node>, std::greater<Node>> open;
+  //processed nodes
   std::vector<bool> closed(cols * rows, false);
   sf::Vector2f goalCell(static_cast<float>(cellGoalX), static_cast<float>(cellGoalY));
   sf::Vector2f startCell(static_cast<float>(cellX), static_cast<float>(cellY));
 
   float hCostStart = heuristic(goalCell,startCell);
-
+  //push goal node to the start of the list 
   open.push(Node(goalCell, 0.f, hCostStart));
-
+  
   while (!open.empty()) {
+	  //always process the node with the lowest f cost
 	  Node current = open.top();
 
 	  open.pop();
 
 	  int currentIndex = static_cast<int>(current.cell.y) * cols + static_cast<int>(current.cell.x);
-
+	  //if its clised then skip
 	  if (closed[currentIndex])
 		  continue;
-	  closed[currentIndex] = true;
-
+	  closed[currentIndex] = true; //mark it closed
 	  for (auto& neighbour : getNeighbours(current.cell)) {
+		  //for each neighbour get current valdi neighbour
 
 		  int neighbourX = static_cast<int>(neighbour.x);
 		  int neighbourY = static_cast<int>(neighbour.y);
 		  int neighbourIndex = neighbourY * cols + neighbourX;
-
+		  //if its an obstacle or closed skip
 		  if (m_costs[neighbourIndex] >= 9999.f || closed[neighbourIndex]) {
 			  continue;
 		  }
+		  // cost evaluation to reach neighbout
 		  sf::Vector2f offset = neighbour - current.cell;
 		  float euclideanDistance = MathUtils::vectorLength(offset) * cellSize;
 		  float newCost = m_integration[currentIndex] + m_costs[neighbourIndex] + euclideanDistance;
-
+		  //if its lower update and add to open list
 		  if (newCost < m_integration[neighbourIndex]) {
 			  m_integration[neighbourIndex] = newCost;
 			  float hCost = heuristic(neighbour, startCell);
@@ -477,22 +485,28 @@ void FlowField::makeIntegrationFieldAStar()
 
 void FlowField::makeFlowField()
 {
+	//starts with no direction
 	m_directions.resize(cols * rows, sf::Vector2f(0.f, 0.f));
+	//for each cell find lowest cost neighbour and set direction to it
 	for (int y = 0; y < rows; ++y)
 		for (int x = 0; x < cols; ++x) {
 			int index = y * cols + x;
+			//set the lowest cost to current cell
 			float lowestCost = m_integration[index];
+
 			sf::Vector2f bestDirection(0.f, 0.f);
+			//check neighbours with lowest cost
 			std::vector<sf::Vector2f> neighbours = getNeighbours(sf::Vector2f((float)x, (float)y));
 			for (int i = 0; i < neighbours.size(); ++i) {
 				int neighbourX = (int)neighbours[i].x;
 				int neighbourY = (int)neighbours[i].y;
 				int neighbourIndex = neighbourY * cols + neighbourX;
 				float value = m_integration[neighbourIndex];
-
+				//if its lower update direction
 				if (value < lowestCost) {
 
 					lowestCost = value;
+					//direction from current to neighbour and the next
 					sf::Vector2f current((x + 0.5f) * cellSize, (y + 0.5f) * cellSize);
 					sf::Vector2f next((neighbourX + 0.5f) * cellSize, (neighbourY + 0.5f) * cellSize);
 					bestDirection = MathUtils::normalize(next - current);
@@ -535,6 +549,7 @@ void FlowField::setGoalPosition(sf::Vector2f position)
 
 void FlowField::updatePath()
 {
+	//updates path taken by circle
 	float cellXf = m_circle.getPosition().x / cellSize;
 	float cellYf = m_circle.getPosition().y / cellSize;
 	sf::Vector2f currentCell{ std::floor(cellXf), std::floor(cellYf) };
