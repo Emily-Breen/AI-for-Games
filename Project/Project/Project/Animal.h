2a6542e1b2ea39423ad8b19631bd8acf4fc380a9
Stephen Dunne <C00194821@setu.ie>
2025-11-19 22:59:30 +0000

    tile movement functionality added. added visualiser to validTiles for UX improvement. need to fix frog logic and weird bug with unplaced tiles vector changing textures

--- Project/Project/Project/Animal.h
+++ Project/Project/Project/Animal.h
@@ -23,11 +23,16 @@ class Animal
 	  std::string getName() const;
 	  bool isEmpty() const;
 	  Player getOwner();
+	  AnimalType getType();
 	  void setPosition(const sf::Vector2f& position);
 	  void initAnimalTexture(float cellSize);
 	  void draw(sf::RenderWindow& m_window);
 	  void rescale(float cellSize);
 	  sf::FloatRect getBounds() const;
+
+	  // New method to get valid moves for the current animal from a given position
+	  std::vector<sf::Vector2i> getValidMoves(int currentRow, int currentCol,
+		  const Animal* grid, int boardSize) const;
 
 private:
 	Player m_owner;
@@ -35,6 +40,14 @@ private:
 	sf::Texture m_texture;
 	sf::Sprite m_sprite{ m_texture };
 
+	// Helper methods for movement validation
+	bool isValidPosition(int row, int col, int boardSize) const;
+
+	// Adds cardinal movements (up, down, left, right) to the moves vector
+	void addAdjacentMoves(int row, int col, const Animal* grid, int boardSize,
+		std::vector<sf::Vector2i>& moves, bool includeDiagonal) const;
+	void addFrogJumpMoves(int row, int col, const Animal* grid, int boardSize,
+		std::vector<sf::Vector2i>& moves) const;
 
 };
 
